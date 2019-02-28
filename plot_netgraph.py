933cd9c781e8122bacf6bc4bd3638ef7f112fa08
deepinx <xcliu1893@gmail.com>
2019-02-28 19:58:47 +0800

    add net graph plotting

---
+++ plot_netgraph.py
@@ -0,0 +1,17 @@
+import logging
+import mxnet as mx
+import numpy as np
+from symbol import get_shufflenet
+from fshufflenetv2 import get_shufflenet_v2
+from fmobilefacenet import get_mobilefacenet
+
+shufflenet = get_shufflenet()
+# shufflenet = get_shufflenet_v2()
+# shufflenet = get_mobilefacenet()
+
+# save as symbol
+sym = shufflenet
+
+## plot network graph
+mx.viz.print_summary(sym, shape={'data':(8,3,112,112)})
+mx.viz.plot_network(sym, shape={'data':(8,3,112,112)}, node_attrs={'shape':'oval','fixedsize':'fasl==false'}).view()
