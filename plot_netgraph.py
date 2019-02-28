12bb06cba122dee06ffa88a4b779dde69e159fcd
deepinx <xcliu1893@gmail.com>
2019-02-28 20:12:31 +0800

    tiny fix

--- plot_netgraph.py
+++ plot_netgraph.py
@@ -5,13 +5,13 @@ from symbol import get_shufflenet
 from fshufflenetv2 import get_shufflenet_v2
 from fmobilefacenet import get_mobilefacenet
 
-shufflenet = get_shufflenet()
-# shufflenet = get_shufflenet_v2()
+# shufflenet = get_shufflenet()
+shufflenet = get_shufflenet_v2()
 # shufflenet = get_mobilefacenet()
 
 # save as symbol
 sym = shufflenet
 
 ## plot network graph
-mx.viz.print_summary(sym, shape={'data':(8,3,112,112)})
-mx.viz.plot_network(sym, shape={'data':(8,3,112,112)}, node_attrs={'shape':'oval','fixedsize':'fasl==false'}).view()
+mx.viz.print_summary(sym, shape={'data':(8,3,28,28)})
+mx.viz.plot_network(sym, shape={'data':(8,3,28,28)}, node_attrs={'shape':'oval','fixedsize':'fasl==false'}).view()
